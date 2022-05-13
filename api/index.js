28b68627bfca34d9263e25e75bcb1d8208be4b30
Omar Brikaa <brikaaomar@gmail.com>
2022-05-13 16:01:16 +0200

    Initial sign up with api

--- api/index.js
+++ api/index.js
@@ -1,17 +1,32 @@
 const express = require('express');
+const cors = require('cors');
 const mysql = require('mysql');
 const config = require('./config');
 const db_config = require('../db/config');
+const body_parser = require('body-parser');
+
 const app = express();
 const port = config.port;
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({ extended: true }));
+app.use(cors())
 
-const db = mysql.createConnection(db_config.connection_options);
+const create_connection = () => {
+    return mysql.createConnection(db_config.connection_options);
+};
 
 app.get('/', (req, res) => {
+    const db = create_connection();
     db.query('select * from users;', (error, results, fields) => {
         res.send(fields);
-        console.log(error);
     });
+    db.end();
+});
+
+app.post('/signup', (req, res) => {
+    const { username, email, password } = req.body;
+    console.log(req.body);
+    res.status(200).send();
 });
 
 app.listen(port, () => {
--- front/signup/index.js
+++ front/signup/index.js
@@ -1,12 +1,26 @@
-(() => {
+import * as config from '../config.js';
+
+(async () => {
     const signup_button = document.getElementById("signup_button");
-    signup_button.addEventListener("click", signup);
-    
+    const username_field = document.getElementById('username');
+    const email_field = document.getElementById('email');
+    const password_field = document.getElementById('password');
 
-    function signup() {
-        let username = document.getElementById("username").value;
-        let password = document.getElementById("password").value;
-        let email = document.getElementById("email").value;
-
-    }
+    signup_button.addEventListener('click', async () => {
+        const username = username_field.value;
+        const email = email_field.value;
+        const password = password_field.value;
+        await fetch(config.api_url + '/signup', {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            mode: 'cors'
+        });
+    })
 })();
