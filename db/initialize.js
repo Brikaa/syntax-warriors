dbfddb1d119a8f290793772adf37de01e1a08280
Omar Brikaa <brikaaomar@gmail.com>
2022-05-15 13:20:52 +0200

    Only delete migrations file if exists

--- db/initialize.js
+++ db/initialize.js
@@ -1,13 +1,15 @@
 const mysql = require('mysql');
 const config = require('./config');
-const fs = require('fs/promises');
+const fs = require('fs');
 
 (async () => {
     console.log('Removing migrations tracker file');
-    await fs.unlink('./all_migrations.txt');
+    if (fs.existsSync('./all_migrations.txt')) {
+        fs.unlinkSync('./all_migrations.txt');
+    }
     delete config.connection_options.database;
     const con = mysql.createConnection({ ...config.connection_options, multipleStatements: true });
-    const sql_statements = await fs.readFile('./initialize.sql', { encoding: 'utf-8' });
+    const sql_statements = fs.readFileSync('./initialize.sql', { encoding: 'utf-8' });
 
     console.log('Performing initial db sql statements');
     con.query(sql_statements, () => {
