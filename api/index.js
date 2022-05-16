7844612ce28b2e4b656a7a06b58a04db8a0ad071
Omar Brikaa <brikaaomar@gmail.com>
2022-05-16 15:16:07 +0200

    Validate user info initial

--- api/index.js
+++ api/index.js
@@ -28,34 +28,67 @@ const handle_api_errors = (e, res) => {
     return res.status(500);
 };
 
-const check_missing_properties = (req_body, required_properties) => {
-    const missing_properties = required_properties.filter((p) => req_body.hasOwnProperty(p));
-    console.log({ req_body, required_properties, missing_properties });
-    if (missing_properties.length > 0) {
-        throw new RequestBodyException(
-            'Missing request body properties: ' + missing_properties.join(', ')
-        );
+const validate_data_types = (data, types) => {
+    if (data.length !== types.length) {
+        throw new Error('Provided data does not match types array');
     }
+    for (let i = 0; i < types.length; ++i) {
+        if (typeof data[i] !== types[i]) {
+            throw new RequestBodyException(
+                `Invalid data type for ${data[i]}, expected: ${types[i]}`
+            );
+        }
+    }
+};
+
+// User actions
+const validate_user_info = async (user_info) => {
+    const { username, email, password } = user_info;
+    return new Promise(async (resolve, reject) => {
+        try {
+            validate_data_types([username, email, password], ['string', 'string', 'string']);
+        } catch (e) {
+            reject(e);
+        }
+        if (username === '') {
+            reject(new RequestBodyException('The username can not be empty'));
+        }
+        if (password.length < 8) {
+            reject(new RequestBodyException('The password can not be less than 8 characters long'));
+        }
+        if (email === '') {
+            reject(new RequestBodyException('The email can not be empty'));
+        }
+        if (username.match('[^A-Za-z0-9]')) {
+            reject(new RequestBodyException('The username must contain letters or numbers only'));
+        }
+        const same_usernames = await db.query(
+            'select username, email from users where username = ? or email = ?',
+            [username, email]
+        );
+        if (same_usernames.length > 0) {
+            const what_exists = same_usernames[0].email === email ? 'email' : 'username';
+            reject(new RequestBodyException(`This ${what_exists} already exists`));
+        }
+        resolve();
+    });
 };
 
 app.post('/get_user', async (req, res) => {
     try {
-        check_missing_properties(req.body, ['username', 'password']);
-        console.log(req.body);
         const { username, password } = req.body;
+
         if (username === null || password === null) {
             return res.status(200).send({ user: null });
         }
-        if (typeof username !== 'string') {
-            return res.status(400).send('The username must be provided as a string');
-        }
-        if (typeof password !== 'string') {
-            return res.status(400).send('The password must be provided as a string');
-        }
+
+        validate_data_types([username, password], ['string', 'string']);
+
         const users = await db.query(
             'select username, email, no_wins, score from users where username = ? and password = ?',
             [username, password]
         );
+
         if (users.length < 1) {
             return res.status(200).json({ user: null });
         }
@@ -67,33 +100,8 @@ app.post('/get_user', async (req, res) => {
 
 app.post('/signup', async (req, res) => {
     try {
-        check_missing_properties(req.body, ['username', 'email', 'password']);
+        await validate_user_info(req.body);
         const { username, email, password } = req.body;
-        // Validate body data
-        if (typeof password !== 'string' || password.length < 8) {
-            return res
-                .status(400)
-                .send('A string password that is at least 8 characters long must be provided');
-        }
-        if (typeof email !== 'string' || email.length < 1) {
-            return res.status(400).send('A string email address must be provided');
-        }
-        if (typeof username !== 'string' || username.length < 1) {
-            return res.status(400).send('A string username must be provided');
-        }
-        if (username.match('[^A-Za-z0-9]')) {
-            return res.status(400).send('The username must contain letters and/or numbers only');
-        }
-
-        // Create user if doesn't exist
-        const same_usernames = await db.query(
-            'select username, email from users where username = ? or email = ?',
-            [username, email]
-        );
-        if (same_usernames.length > 0) {
-            const what_exists = same_usernames[0].email === email ? 'email' : 'username';
-            return res.status(400).send(`This ${what_exists} already exists`);
-        }
         await db.query('insert into users (email, username, password) values (?, ?, ?)', [
             email,
             username,
