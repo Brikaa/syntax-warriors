3abfba6ab3750335d4d0b40141f508718b473a58
Omar Brikaa <brikaaomar@gmail.com>
2022-05-17 18:39:48 +0200

    Create contest initial front-end

---
+++ front/admin/create_contest/index.js
@@ -0,0 +1,8 @@
+(() => {
+    const contests_elements = {
+        name: document.getElementById('name'),
+        description: document.getElementById('description')
+    };
+    document.getElementById('submit').addEventListener(() => {
+    });
+})();
