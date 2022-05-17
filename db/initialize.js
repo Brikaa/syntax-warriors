725bf5e2bb4d6c0d66a2f1dcc25a2bcc6188676c
Omar Brikaa <brikaaomar@gmail.com>
2022-05-17 16:57:31 +0200

    Don't use cd in package.json

--- db/initialize.js
+++ db/initialize.js
@@ -1,18 +1,24 @@
 const mysql = require('mysql');
 const config = require('./config');
 const fs = require('fs');
+const path = require('path');
 
 (async () => {
     console.log('Removing migrations tracker file');
-    if (fs.existsSync('./all_migrations.txt')) {
-        fs.unlinkSync('./all_migrations.txt');
+    if (fs.existsSync(path.join(__dirname, 'all_migrations.txt'))) {
+        fs.unlinkSync(path.join(__dirname, 'all_migrations.txt'));
     }
     delete config.connection_options.database;
     const con = mysql.createConnection({ ...config.connection_options, multipleStatements: true });
-    const sql_statements = fs.readFileSync('./initialize.sql', { encoding: 'utf-8' });
+    const sql_statements = fs.readFileSync(path.join(__dirname, 'initialize.sql'), {
+        encoding: 'utf-8'
+    });
 
     console.log('Performing initial db sql statements');
-    con.query(sql_statements, () => {
+    con.query(sql_statements, (e) => {
+        if (e) {
+            throw e;
+        }
         console.log('Done');
     });
     con.end();
--- db/migrate.js
+++ db/migrate.js
@@ -1,8 +1,9 @@
 const mysql = require('mysql');
 const fs = require('fs');
 const config = require('./config');
+const path = require('path');
 
-const migrations_file_path = './all_migrations.txt';
+const migrations_file_path = path.join(__dirname, 'all_migrations.txt');
 
 (async () => {
     const con = mysql.createConnection({ ...config.connection_options, multipleStatements: true });
@@ -14,14 +15,16 @@ const migrations_file_path = './all_migrations.txt';
         applied_migrations = fs.readFileSync(migrations_file_path).toString().split('\n');
     }
 
-    const all_migrations = fs.readdirSync('./migrations');
+    const all_migrations = fs.readdirSync(path.join(__dirname, './migrations'));
 
     for (const migration of all_migrations) {
         if (applied_migrations.includes(migration)) {
             console.log(`Already applied ${migration}`);
             continue;
         }
-        const migration_statements = fs.readFileSync('migrations/' + migration).toString();
+        const migration_statements = fs
+            .readFileSync(path.join(__dirname, 'migrations/', migration))
+            .toString();
         con.query(migration_statements, (error) => {
             if (error) {
                 throw error;
